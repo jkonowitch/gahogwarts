16ccb51ab79a52ea1ca7a966130c5804c03a3bef
Jeffrey Konowitch <jkonowitch@gmail.com>
2013-08-13 18:58:09 -0400

    use rails link helpers and correct route -> use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Hogwarts::Application.routes.draw do
   root to: 'welcome#index'
 
-  get '/houses' => 'houses#List'
-  get '/students' => 'students#index'
-  get '/students' => 'students#creat'
+  resource :houses
+  resource :students
 end
